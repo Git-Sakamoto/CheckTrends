2195b35e81df3e42ee0d01fa61ba148e79312b42
Git-Sakamoto <sakakazutouroku@gmail.com>
2021-01-27 07:54:27 +0900

    update

--- app/src/main/java/com/example/checktrends/MainActivity.java
+++ app/src/main/java/com/example/checktrends/MainActivity.java
@@ -3,7 +3,6 @@ package com.example.checktrends;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.fragment.NavHostFragment;
--- app/src/main/java/com/example/checktrends/bookmark/BookMarkFragment.java
+++ app/src/main/java/com/example/checktrends/bookmark/BookMarkFragment.java
@@ -4,15 +4,19 @@ import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.NavigationUI;
 
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.checktrends.MainActivity;
 import com.example.checktrends.R;
 
 public class BookMarkFragment extends Fragment {
@@ -34,15 +38,22 @@ public class BookMarkFragment extends Fragment {
     }
 
     @Override
-    public void onPrepareOptionsMenu(@NonNull Menu menu) {
-        super.onPrepareOptionsMenu(menu);
+    public void onCreateOptionsMenu(@NonNull Menu menu, @NonNull MenuInflater inflater) {
+        inflater.inflate(R.menu.menu_bookmark,menu);
     }
 
     @Override
-    public void onCreateOptionsMenu(@NonNull Menu menu, @NonNull MenuInflater inflater) {
-        menu.clear();
-        inflater.inflate(R.menu.menu_bookmark,menu);
-        System.out.println("test");
-    }
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.menu_register:
+                System.out.println("test");
+                break;
+        }
 
+        /*NavController navController = Navigation.findNavController(getActivity(), R.id.nav_host_fragment);
+        return NavigationUI.onNavDestinationSelected(item, navController)
+                || super.onOptionsItemSelected(item);*/
+
+        return super.onOptionsItemSelected(item);
+    }
 }
